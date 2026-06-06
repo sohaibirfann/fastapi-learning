e7493626077813b1dffc1f6d1564fb5e5610cd73
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-06 20:55:19 +0530

    Add first POST endpoint

--- Project1/books.py
+++ Project1/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -49,3 +49,8 @@ async def read_authot_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
 
     return books_to_return
+
+
+@app.post("/books/{create_book}")
+async def add_book(new_book=Body()):
+    BOOKS.append(new_book)
