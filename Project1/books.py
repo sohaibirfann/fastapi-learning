ab5ba01601fbe019223844651eb06bf5879c3975
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-06 16:30:51 +0530

    Learned query parameters

--- Project1/books.py
+++ Project1/books.py
@@ -4,10 +4,10 @@ app = FastAPI()
 
 BOOKS = [
     {"title": "Title One", "author": "Author One", "category": "Science"},
-    {"title": "Title Two", "author": "Author Two", "category": "English"},
+    {"title": "Title Two", "author": "Author Two", "category": "Science"},
     {"title": "Title Three", "author": "Author Three", "category": "Math"},
     {"title": "Title Four", "author": "Author Four", "category": "Math"},
-    {"title": "Title Five", "author": "Author Two", "category": "Computer"},
+    {"title": "Title Five", "author": "Author Two", "category": "Science"},
 ]
 
 
@@ -16,9 +16,9 @@ async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{dynamic_param}")
-async def read_dynamic(dynamic_param: str):
-    return {"dynamic_param": dynamic_param}
+# @app.get("/books/{dynamic_param}")
+# async def read_dynamic(dynamic_param: str):
+#     return {"dynamic_param": dynamic_param}
 
 
 @app.get("/books/{book_title}")
@@ -26,3 +26,26 @@ async def read_book(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():
             return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_authot_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
+            books_to_return.append(book)
+
+    return books_to_return
