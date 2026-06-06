139180a90892d933ce9a249ae2f941b7602c3991
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-06 16:00:30 +0530

    Learned path parameters and dynamic params

--- Project1/books.py
+++ Project1/books.py
@@ -2,6 +2,27 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/test")
-async def first_api():
-    return {"message": "First Endpoint"}
+BOOKS = [
+    {"title": "Title One", "author": "Author One", "category": "Science"},
+    {"title": "Title Two", "author": "Author Two", "category": "English"},
+    {"title": "Title Three", "author": "Author Three", "category": "Math"},
+    {"title": "Title Four", "author": "Author Four", "category": "Math"},
+    {"title": "Title Five", "author": "Author Two", "category": "Computer"},
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{dynamic_param}")
+async def read_dynamic(dynamic_param: str):
+    return {"dynamic_param": dynamic_param}
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
