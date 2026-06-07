a5998f6bff6e02aff5abab4c8cc77ec54ddc89d7
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-07 19:34:39 +0530

    learned about pydantic configs and setup first config

--- Project2/books2.py
+++ Project2/books2.py
@@ -21,11 +21,23 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1, max_length=50)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    # defining a model config
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new Book",
+                "author": "CodeItOut",
+                "description": "Description of a book",
+                "rating": 5,
+            }
+        }
+    }
 
 
 BOOKS = [
