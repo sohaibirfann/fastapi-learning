7c0159ac3363fd7042a85426f75a9b8347cc8e78
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-07 20:22:50 +0530

    Learned more validation and working with different types of requests

--- Project2/books2.py
+++ Project2/books2.py
@@ -11,13 +11,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -26,6 +28,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1, max_length=50)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     # defining a model config
     model_config = {
@@ -35,24 +38,45 @@ class BookRequest(BaseModel):
                 "author": "CodeItOut",
                 "description": "Description of a book",
                 "rating": 5,
+                "published_date": 2000,
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, "Computer Science", "CodeItOut", "Good book I Guess", 5),
-    Book(2, "Be Fast with FastAPI", "CodeItOut", "Great book I Guess", 5),
-    Book(3, "Master Endpoints", "CodeItOut", "Awesome book I Guess", 5),
-    Book(4, "HP1", "Author 1", "Boilerplate Desc", 2),
-    Book(5, "HP2", "Author 2", "Boilerplate Desc", 3),
-    Book(6, "HP3", "Author 3", "Boilerplate Desc", 1),
+    Book(1, "Computer Science", "CodeItOut", "Good book I Guess", 5, 2000),
+    Book(2, "Be Fast with FastAPI", "CodeItOut", "Great book I Guess", 5, 2000),
+    Book(3, "Master Endpoints", "CodeItOut", "Awesome book I Guess", 5, 2012),
+    Book(4, "HP1", "Author 1", "Boilerplate Desc", 2, 2012),
+    Book(5, "HP2", "Author 2", "Boilerplate Desc", 3, 2025),
+    Book(6, "HP3", "Author 3", "Boilerplate Desc", 1, 2025),
 ]
 
 
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+# fetch book by id
+@app.get("/books/{book_id}")
+async def fetch_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+# fetch books by rating
+@app.get("/books/")
+async def fetch_books_by_rating(book_rating: int):
+    return [book for book in BOOKS if book_rating == book.rating]
+
+
+# fetch books by published date
+@app.get("/books/publish_date/")
+async def fetch_books_by_date(date: int):
+    return [book for book in BOOKS if date == book.published_date]
 
 
 @app.post("/create-book")
@@ -63,9 +87,22 @@ async def create_book(book_request: BookRequest):
 
 # function to auto-increment id no.
 def find_book_id(book: Book):
-    if len(BOOKS) > 0:
-        book.id = BOOKS[-1].id + 1
-    else:
-        book.id = 1
-
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
+
+
+# update a book
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+# delete a book
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
