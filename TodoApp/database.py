adc0001785266e85078cc2111013bf59b5f05d92
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-14 16:03:09 +0530

    fetching by id

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -8,6 +8,6 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
-from database import engine
+from fastapi import FastAPI, Depends, HTTPException, Path
+from database import engine, SessionLocal
+from starlette import status
 import models
+from models import Todos
+
+from sqlalchemy.orm import Session
+from typing import Annotated
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="Todo not found")
