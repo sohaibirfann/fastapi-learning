6e4be5c4fa2b0014057a351c1994f2e8634d8df9
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-20 18:13:37 +0530

    add auth token login support

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -6,8 +6,13 @@ from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 router = APIRouter()
+
+SECRET_KEY = "76B2826F687C0662689A1726082B9EB3"
+ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -32,6 +37,18 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta)
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserReequest):
     create_user_model = Users(
@@ -45,3 +62,14 @@ async def create_user(db: db_dependency, create_user_request: CreateUserReequest
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+
+    return "Successful Authentication"
