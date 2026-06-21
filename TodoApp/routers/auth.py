d364d690a59f4e028c1dcf0de8c33eaf81ed1441
Sohaib Irfan <tihor.personal@gmail.com>
2026-06-21 10:03:37 +0530

    Add requirements.txt file and implement JWT token/login response

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta, timezone
+
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from models import Users
@@ -25,6 +27,10 @@ class CreateUserReequest(BaseModel):
     password: str
     role: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
 
 def get_db():
     db = SessionLocal()
@@ -43,10 +49,14 @@ def authenticate_user(username: str, password: str, db):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta)
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
@@ -64,12 +74,13 @@ async def create_user(db: db_dependency, create_user_request: CreateUserReequest
     db.commit()
 
 
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return "Failed Authentication"
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
 
-    return "Successful Authentication"
+    return {'access_token': token, 'token_type': 'barer'}
